fd7b0d0ac49bf69bf40c84f1107b2e7828c37ba7
NaufalMWaliy <nmuntaaza.wh@gmail.com>
2018-08-13 21:40:56 +0700

    test 2

--- resources/views/pages/welcome.blade.php
+++ resources/views/pages/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Welcome
+                    Welcome View
                 </div>
 
                 <div class="links">
