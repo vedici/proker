f70c8f2fa6ca9a037b76b85dfe896eb99046ac96
kivlanaziz <31475837+kivlanaziz@users.noreply.github.com>
2018-09-13 16:07:32 +0700

    not rounded card

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -3,8 +3,8 @@
 @section('content')
 <div class="container" style="margin-top:95px;">
     <div class="row justify-content-center align-items-center">
-        <div class="col-md-8">
-            <div class="card">
+        <div class="col-md-6">
+            <div class="card rounded-0">
                 <div class="card-header">{{ __('Login') }}</div>
 
                 <div class="card-body">
