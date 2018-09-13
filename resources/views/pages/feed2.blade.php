5ff4c94910598f671b15e17306330498e986f941
kivlanaziz <31475837+kivlanaziz@users.noreply.github.com>
2018-09-13 11:30:32 +0700

    update UI feed

--- resources/views/pages/feed2.blade.php
+++ resources/views/pages/feed2.blade.php
@@ -17,8 +17,93 @@
                         </div>
                     </div>
                 </div>
+                <div class="row" style="margin-top: 50px;">
+                    <!-- Card Company Info-->
+                    <div class="col-md-4">
+                      <div class="card rounded-0">
+                        <div class="card-header text-center">
+                          <h4>Company Info</h4>
+                        </div>
+                        <div class="card-body">
+                          <table class="table">
+                            <tbody>
+                              <tr>
+                                <th>Sales Name</th>
+                                <td>Denning & fourcade</td>
+                              </tr>
+                              <tr>
+                                <th>Phone Number</th>
+                                <td>089532718927</td>
+                              </tr>
+                              <tr>
+                                <th>Address</th>
+                                <td>Salem Boulevard No. 15, Bouyard Avenue</td>
+                              </tr>
+                            </tbody>
+                          </table>
+                        </div>
+                      </div>
+                    </div>
+                    <!---->
+                    <!-- Card Posting -->
+                    <div class="col-md-6">
+                      <!-- List Posting -->
+                      <div class="card rounded-0" style="margin-bottom:15px;">
+                        <div class="card-body">
+                          <div class="row" style="border-bottom:solid 1px #d4d7db; padding:5px;">
+                            <div class="col-md-2">
+                              <img src="https://as.ftcdn.net/r/v1/pics/ea2e0032c156b2d3b52fa9a05fe30dedcb0c47e3/landing/images_photos.jpg" class="img-responsive" style="width:50px;height:50px;"/>
+                            </div>
+                            <div class="col-md-10">
+                              <span class="">Denning & Fourcade</span>
+                            </div>
+                          </div>
+                          <div class="row" style="margin-top:10px; padding:5px;">
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur
+                          </div>
+                        </div>
+                      </div>
+
+                      <div class="card rounded-0" style="margin-bottom:15px;">
+                        <div class="card-body">
+                          <div class="row" style="border-bottom:solid 1px #d4d7db; padding:5px;">
+                            <div class="col-md-2">
+                              <img src="https://as.ftcdn.net/r/v1/pics/ea2e0032c156b2d3b52fa9a05fe30dedcb0c47e3/landing/images_photos.jpg" class="img-responsive" style="width:50px;height:50px;"/>
+                            </div>
+                            <div class="col-md-10">
+                              <span class="">Denning & Fourcade</span>
+                            </div>
+                          </div>
+                          <div class="row" style="margin-top:10px; padding:5px;">
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur
+                          </div>
+                        </div>
+                      </div>
+
+                      <div class="card rounded-0" style="margin-bottom:15px;">
+                        <div class="card-body">
+                          <div class="row" style="border-bottom:solid 1px #d4d7db; padding:5px;">
+                            <div class="col-md-2">
+                              <img src="https://as.ftcdn.net/r/v1/pics/ea2e0032c156b2d3b52fa9a05fe30dedcb0c47e3/landing/images_photos.jpg" class="img-responsive" style="width:50px;height:50px;"/>
+                            </div>
+                            <div class="col-md-10">
+                              <span class="">Denning & Fourcade</span>
+                            </div>
+                          </div>
+                          <div class="row" style="margin-top:10px; padding:5px;">
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur
+                          </div>
+                        </div>
+                      </div>
+                      <!---->
+                    </div>
+                    <!---->
+                    <div class="col-md-2">
+
+                    </div>
+                </div>
             </div>
-                
+
         </div>
     </div>
 </div>
